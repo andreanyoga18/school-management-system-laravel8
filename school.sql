da435cb1484d770098c99e1911f612f1e5e8e27e
andreanyoga18 <andreanyoga18@gmail.com>
2023-01-13 07:36:12 +0700

    third commit

---
+++ school.sql
@@ -0,0 +1,449 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 13, 2023 at 01:23 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fee_categories`
+--
+
+CREATE TABLE `fee_categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `fee_categories`
+--
+
+INSERT INTO `fee_categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Category 1', '2023-01-11 20:17:03', '2023-01-11 20:17:03'),
+(2, 'Category 2', '2023-01-11 20:21:39', '2023-01-11 20:21:39'),
+(3, 'Category 3', '2023-01-11 20:21:47', '2023-01-11 20:21:47'),
+(5, 'Category 4', '2023-01-11 20:51:50', '2023-01-11 20:56:30'),
+(6, 'Category 5', '2023-01-11 20:51:58', '2023-01-11 20:51:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fee_category_amounts`
+--
+
+CREATE TABLE `fee_category_amounts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `fee_category_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `amount` double NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `fee_category_amounts`
+--
+
+INSERT INTO `fee_category_amounts` (`id`, `fee_category_id`, `class_id`, `amount`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 200, '2023-01-12 00:14:43', '2023-01-12 00:14:43'),
+(2, 1, 2, 250, '2023-01-12 00:14:43', '2023-01-12 00:14:43'),
+(3, 1, 3, 300, '2023-01-12 00:14:43', '2023-01-12 00:14:43'),
+(4, 2, 1, 900, '2023-01-12 00:35:47', '2023-01-12 00:35:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2014_10_12_200000_add_two_factor_columns_to_users_table', 1),
+(4, '2019_08_19_000000_create_failed_jobs_table', 1),
+(5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(6, '2023_01_05_012740_create_sessions_table', 1),
+(7, '2014_10_12_000000_create_users_table', 2),
+(8, '2023_01_11_013534_create_student_classes_table', 3),
+(9, '2023_01_11_042729_create_student_years_table', 4),
+(10, '2023_01_11_065731_create_student_groups_table', 5),
+(11, '2023_01_11_073707_create_student_shifts_table', 6),
+(12, '2023_01_12_022721_create_fee_categories_table', 7),
+(13, '2023_01_12_040202_create_fee_category_amounts_table', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` text NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('pTTyAmRhr0HlbXT5fWYpp32hl6ISmBhefqIotkVm', 14, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoibWIxdWRrWWdqbnFUalpsVU5XZXlXVGQzcjZncVd1bzhSTzlxRWQ4MSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTQ7czoyMToicGFzc3dvcmRfaGFzaF9zYW5jdHVtIjtzOjYwOiIkMnkkMTAkcml6a2xOVHJ0NC5oZEZhMXBkdDMxTzREakZsZDMuMXc0VnZOMDhRc2QyNmg1LmRmTTlJWlMiO3M6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjQ0OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvc2V0dXBzL2ZlZS9hbW91bnQvdmlldyI7fX0=', 1673509796),
+('XjtaUg7Jf10f2Jk8DbIOf8djKex2z7lnTmWb1rEw', 14, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoidEpUa0RBM1pJelZmeHlaZmFRd0gwVWFVREZlMElMS0FsRkJtV1JvNyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTQ7czoyMToicGFzc3dvcmRfaGFzaF9zYW5jdHVtIjtzOjYwOiIkMnkkMTAkcml6a2xOVHJ0NC5oZEZhMXBkdDMxTzREakZsZDMuMXc0VnZOMDhRc2QyNmg1LmRmTTlJWlMiO3M6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjM1OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvcHJvZmlsZXMvZWRpdCI7fX0=', 1673534109);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_classes`
+--
+
+CREATE TABLE `student_classes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `student_classes`
+--
+
+INSERT INTO `student_classes` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Class One', '2023-01-10 19:08:36', '2023-01-10 19:08:36'),
+(2, 'Class Two', '2023-01-10 19:09:17', '2023-01-10 19:09:17'),
+(3, 'Class Three', '2023-01-10 19:09:26', '2023-01-10 19:09:26'),
+(4, 'Class Satu', '2023-01-10 20:47:00', '2023-01-10 20:47:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_groups`
+--
+
+CREATE TABLE `student_groups` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `student_groups`
+--
+
+INSERT INTO `student_groups` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Group 1', '2023-01-11 00:15:59', '2023-01-11 20:48:05'),
+(3, 'Group 2', '2023-01-11 00:19:00', '2023-01-11 00:19:00'),
+(4, 'Group 3', '2023-01-11 00:27:44', '2023-01-11 00:27:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_shifts`
+--
+
+CREATE TABLE `student_shifts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `student_shifts`
+--
+
+INSERT INTO `student_shifts` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Shift 1123123', '2023-01-11 00:50:59', '2023-01-11 00:57:33'),
+(2, 'Shift 21231231', '2023-01-11 00:52:17', '2023-01-11 19:21:37'),
+(5, 'Shift 3', '2023-01-11 19:22:08', '2023-01-11 19:22:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_years`
+--
+
+CREATE TABLE `student_years` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `student_years`
+--
+
+INSERT INTO `student_years` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, '2023', '2023-01-10 23:15:18', '2023-01-10 23:15:18'),
+(2, '2015', '2023-01-10 23:15:53', '2023-01-10 23:30:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `usertype` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `status` tinyint(255) DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `current_team_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `profile_photo_path` varchar(2048) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `usertype`, `name`, `email`, `email_verified_at`, `password`, `mobile`, `address`, `gender`, `image`, `status`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
+(2, 'Admin', 'yoga1', '11yoga@gmail.com', NULL, '$2y$10$22kBPUHNevtoSSaB82UW/eMF.qPTpAXvGZg4hZ9CwOTc42WhHby7O', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-01-08 23:47:20', '2023-01-10 00:37:45'),
+(13, 'Admin', 'yoga2', 'yoga2@gmail.com', NULL, '$2y$10$NK3ZbOxIjhLr147aHR8HE.EbVNXnXGfUTvP0nKSO.Z8XPeH/WObUq', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-01-10 00:37:20', '2023-01-10 07:10:38'),
+(14, 'Admin', 'Yoga', 'javaisme99@gmail.com', NULL, '$2y$10$rizklNTrt4.hdFa1pdt31O4DjFld3.1w4VvN08Qsd26h5.dfM9IZS', '081237123512', 'Kazakhstan', 'Male', NULL, NULL, NULL, NULL, NULL, '2023-01-10 06:37:31', '2023-01-10 17:42:18'),
+(15, 'User', 'Budi', 'budi1@gmail.com', NULL, '$2y$10$okZqGfNvX6qUn4IKYiMIg.qnLGxXl/zORr0awzO63IjYMD5IgO.nW', '08123123512', 'Gemolong', 'Male', NULL, NULL, NULL, NULL, NULL, '2023-01-10 17:53:35', '2023-01-10 17:59:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `fee_categories`
+--
+ALTER TABLE `fee_categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `fee_categories_name_unique` (`name`);
+
+--
+-- Indexes for table `fee_category_amounts`
+--
+ALTER TABLE `fee_category_amounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indexes for table `student_classes`
+--
+ALTER TABLE `student_classes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_classes_name_unique` (`name`);
+
+--
+-- Indexes for table `student_groups`
+--
+ALTER TABLE `student_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_groups_name_unique` (`name`);
+
+--
+-- Indexes for table `student_shifts`
+--
+ALTER TABLE `student_shifts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_shifts_name_unique` (`name`);
+
+--
+-- Indexes for table `student_years`
+--
+ALTER TABLE `student_years`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_years_name_unique` (`name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fee_categories`
+--
+ALTER TABLE `fee_categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `fee_category_amounts`
+--
+ALTER TABLE `fee_category_amounts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_classes`
+--
+ALTER TABLE `student_classes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student_groups`
+--
+ALTER TABLE `student_groups`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student_shifts`
+--
+ALTER TABLE `student_shifts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `student_years`
+--
+ALTER TABLE `student_years`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
